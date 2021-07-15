22040b316831ca101cd7b63d1588ce348d1c4cb6
sjvrensburg <sjansevanrensburg@gmail.com>
2021-07-15 16:06:57 +0200

    Removed the lossless preset since it was terrible. Also, remove options from the slides preset since they don't work with the new version of moviepy.

--- src/stts/presets.py
+++ src/stts/presets.py
@@ -1,15 +1,7 @@
 
 # Presets:
-# Export at highest quality for additional
-# processing
-# TODO: This is actually terrible
-lossless = {
-    "codec": "mpeg4", # "png",
-    "preset": "ultrafast",
-    "threads": 4}
-
-# Encode using the settings that I use for
-# slide decks in shotcut.
+# Settings suitable for a slide deck. Based on that
+# in shotcut.
 slides = {
     "fps": 30,
     "codec": "libx264",
@@ -22,10 +14,8 @@ slides = {
     "audio_bitrate": '80k',
     "audio_bufsize": 2000,
     "temp_audiofile": None,
-    "rewrite_audio": True,
     "remove_temp": True,
     "write_logfile": False,
-    "verbose": True,
     "threads": 4,
     "ffmpeg_params": ['-crf', '27', '-movflags', '+faststart'],
     "logger": 'bar'}
